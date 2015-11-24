62901af1234bd7a064666232ba60bca14e7713c0
2xlink <ben_weller@web.de>
2015-11-24 15:26:49 +0100

    added create_event

---
+++ src/event/eventResource.java
@@ -0,0 +1,45 @@
+package event;
+
+import java.util.ArrayList;
+import java.util.List;
+import entities.User;
+import restRequest.Todo;
+import restRequest.TodoDao;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.UriInfo;
+
+@Path("/event")
+public class eventResource {
+	// Allows to insert contextual objects into the class,
+	// e.g. ServletContext, Request, Response, UriInfo
+	@Context
+	UriInfo uriInfo;
+	@Context
+	Request request;
+
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String getTodosBrowser() {
+		return("test");
+	}
+
+	@POST
+	@Produces(MediaType.TEXT_HTML)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public void createEvent(@FormParam("name") String name, @FormParam("users") String users,
+			@Context HttpServletResponse servletResponse) {
+		System.out.println(name + users);
+	}
+}
