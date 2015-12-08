cd5502771c7888472872575b98aa92a3455d8941
2xlink <ben_weller@web.de>
2015-12-08 17:16:12 +0100

    User Login works!

--- src/restRequest/userResource.java
+++ src/restRequest/userResource.java
@@ -2,6 +2,11 @@ package restRequest;
 
 import entities.*;
 import entities.Error;
+import Crypto.PasswordHash;
+
+import java.security.CryptoPrimitive;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
@@ -17,7 +22,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.UriInfo;
 
-@Path("/event")
+@Path("/user")
 public class userResource {
 	// Allows to insert contextual objects into the class,
 	// e.g. ServletContext, Request, Response, UriInfo
@@ -26,36 +31,45 @@ public class userResource {
 	@Context
 	Request request;
 
-	@GET
-	@Produces(MediaType.TEXT_HTML)
-	public String getTodosBrowser() {
-		return ("test");
+	@Path("login")
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public LoginResponse UserLogin(@FormParam("email") String email, 
+			@FormParam("password") String pass)
+			throws NoSuchAlgorithmException, InvalidKeySpecException {
+		LoginResponse response = new LoginResponse(new Error(), "", "1");
+		String hash_db = SQLConnection.getHashToEmail(email);
+		if (hash_db != null) {
+//			Boolean isValid = Crypto.PasswordHash.validatePassword(pass, hash_db);
+			System.out.println(email+","+pass);
+			Boolean isValid = pass.equals(hash_db);
+			System.out.println(pass + ", " + hash_db + ", " + isValid);
+			if (isValid) {
+				response.setStatus("Ok.");
+			}
+			else {
+				response.setStatus("Invalid password or username.");
+			}
+		} else {
+			response.setStatus("Invalid password or username.");
+		}
+		return response;
 	}
-
-//	@Path("create")
-//	@POST
-//	@Produces(MediaType.TEXT_HTML)
-//	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-//	public String createEvent(@FormParam("email") String email, @FormParam("password") String pw,
-//			@Context HttpServletResponse servletResponse) {
-//		System.out.println(email + pw);
-//		return "{\"email\": " + email + ", \"password\" : " + pw + "}";
-//	}
 
 	@Path("create")
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
-//	@Consumes(MediaType.APPLICATION_JSON)
+	// @Consumes(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	public EventResponse createEventForm(@FormParam("name") String name, @FormParam("users") String users) {
 		// look up user
-		
+
 		Event event = new Event(name, 1, null, "Custom description.", 1);
 		Error error = new Error();
 		return new EventResponse(event, error);
 	}
 
-	
 	@Path("delete")
 	@POST
 	@Produces(MediaType.TEXT_HTML)
@@ -63,7 +77,7 @@ public class userResource {
 	public void deleteEvent(@FormParam("id") Integer id) {
 		System.out.println(id.toString());
 	}
-	
+
 	@Path("get/{id}")
 	@GET
 	@Produces(MediaType.TEXT_HTML)
